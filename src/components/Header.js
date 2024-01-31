569605aeee8990a8657b26699b96e43eb7726b8b
Brandon Aron <brandon.aron38@gmail.com>
2024-01-31 19:20:18 +0200

    Progress on the header as per Figma Design

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,16 @@ import '../styles/Header.css';
 function Header() {
   return (
     <header className="header">
+      {/* Container for title and tagline */}
+      <div className="header-title">
+        <div className="brand-container">
+          ORPHEUS
+        </div>
+        <div className="tagline-container">
+          Step into your next escape
+        </div>
+      </div>
+      
       <div className="header-container">
         <Link to="/" className="header-button">Home</Link>
         <Link to="/destinations" className="header-button">Destinations</Link>
