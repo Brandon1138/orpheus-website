05478764fec15d10b64e7423c0aebf22bc41ce51
Brandon Aron <brandon.aron38@gmail.com>
2024-02-03 21:10:46 +0200

    fixed navigation scale

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,10 +5,8 @@ function Hero() {
   return (
     <div className="hero">
       <div className="hero-content">
-      </div>
-      <div className="empty-space">
-      </div>
       <button className="cta-button">Get Started</button>
+      </div>
     </div>
   );
 }
