9e13657c7e0656cda2511d5884283541e3e8abdd
Brandon Aron <brandon.aron38@gmail.com>
2024-02-05 16:03:46 +0200

    Fixed some padding issues

--- src/components/DestinationsSections.js
+++ src/components/DestinationsSections.js
@@ -6,11 +6,10 @@ function DestinationsSection() {
   return (
     <section className="destinations-section">
       <h1>Destinations</h1>
-      <h3>Discover the wrold, one adventure at a time</h3>
+      <h3>Discover the world, one adventure at a time</h3>
       <p>Explore the most stunning destinations around the world with Orpheus. From tropical beaches to snow-capped mountains, we have hand-picked the best destinations for you to make your next trip unforgettable. Browse through our 9 featured destinations and book your next adventure with just a click!</p>
       <button className="cta-button">Get Started</button>
       <div className="destinations-grid">
-        {/* Add DestinationCard components here */}
       </div>
     </section>
   );
